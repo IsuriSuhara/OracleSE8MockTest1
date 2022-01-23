ae3fbb7279f84ef58eb91fbaf22a2f30d7b20a0e
IsuriSuhara <56144961+IsuriSuhara@users.noreply.github.com>
2022-01-22 23:49:49 -0800

    Create q18.Bozo.java

---
+++ q18.Bozo.java
@@ -0,0 +1,8 @@
+package q18;
+
+public interface Bozo {
+    int type =0;
+    public void jump();//have to give method body when implementing
+    public static void run(){}
+    public default void fly(){}
+}
